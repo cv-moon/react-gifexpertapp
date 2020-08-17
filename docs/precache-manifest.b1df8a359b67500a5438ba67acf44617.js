b2865a830862ce8375f47310fe05209b7af9e44f
Cristian Chuquitarco <cristian.chuquitarco92@gmail.com>
2020-08-17 15:30:51 -0500

    docs agregado

---
+++ docs/precache-manifest.b1df8a359b67500a5438ba67acf44617.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1af09c1141a83c24610638ef653d5b2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c8e189a876976a6156a",
+    "url": "/static/css/main.77b10cc8.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c8e189a876976a6156a",
+    "url": "/static/js/main.494f2545.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
